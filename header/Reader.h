9ac2a0457429b0e40096d3b965a1cdb16c9fa366
WiktorPieklik <wiktor.pieklik@icloud.com>
2020-10-16 00:52:24 +0200

    Created abstract class Reader

---
+++ header/Reader.h
@@ -0,0 +1,21 @@
+//
+// Created by Wiktor Pieklik on 15/10/2020.
+//
+
+#ifndef GPU_KARP_RABIN_READER_H
+#define GPU_KARP_RABIN_READER_H
+
+#include <vector>
+#include <string>
+
+class Reader
+{
+protected:
+    std::vector<std::string> textVector;
+public:
+    explicit Reader(const std::string& text){};
+    virtual std::vector<std::string>& read() = 0;
+    virtual ~Reader() = default;
+};
+
+#endif //GPU_KARP_RABIN_READER_H
