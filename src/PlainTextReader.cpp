322fc51aa8e154feeddadb8d42af087ac3e692eb
WiktorPieklik <wiktor.pieklik@icloud.com>
2020-10-16 00:54:07 +0200

    Created Reader derivative class - PlainTextReader

---
+++ src/PlainTextReader.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Wiktor Pieklik on 15/10/2020.
+//
+
+#include "../header/PlainTextReader.h"
+
+PlainTextReader::PlainTextReader(const std::string &text) : Reader(text)
+{
+    textVector.push_back(text);
+}
+
+std::vector<std::string>& PlainTextReader::read()
+{
+    return textVector;
+}
