6157109c9b3fb78ac44563b59ef0f148bb6b695b
Dawid Zawadzki <lamazlo21@gmail.com>
2020-10-23 11:01:16 +0200

    Implement TextSplitter class Header and Body files

--- header/Utility/TextSplitter.h
+++ header/Utility/TextSplitter.h
@@ -5,8 +5,19 @@
 #ifndef GPU_KARP_RABIN_TEXTSPLITTER_H
 #define GPU_KARP_RABIN_TEXTSPLITTER_H
 
+#include <cmath>
+#include <tuple>
+#include <vector>
 
 class TextSplitter {
+
+private:
+    std::vector<std::tuple<long long int, long long int>> ranges;
+    static const int numOfThreads = 32;
+
+    int rangeWidth(long long int textLength);
+public:
+    std::vector<std::tuple<long long int, long long int>>& splitText(long long int textLength, int patternLength);
 
 };
 
--- src/Utility/TextSplitter.cpp
+++ src/Utility/TextSplitter.cpp
@@ -3,3 +3,24 @@
 //
 
 #include "TextSplitter.h"
+
+int TextSplitter::rangeWidth(long long int textLength) {
+    return ceil((double) textLength / (double) numOfThreads);
+}
+
+std::vector<std::tuple<long long int, long long int>>& TextSplitter::splitText(long long int textLength, int patternLength) {
+    int width = rangeWidth(textLength);
+
+    for(int i = 0; i < textLength; i += width) {
+        if((i + patternLength - 1) < textLength && (i + width - 1 + patternLength - 1) < textLength) {
+            ranges.push_back(std::make_tuple(i, i + width - 1));
+        }
+        else if((i + patternLength - 1) < textLength && ((i + width - 1 + patternLength - 1) >= textLength)) {
+            ranges.push_back(std::make_tuple(i, textLength - patternLength));
+        } else if((i + patternLength - 1) >= textLength) {
+            ranges.push_back(std::make_tuple(-1, -1));
+        }
+    }
+
+    return ranges;
+}
