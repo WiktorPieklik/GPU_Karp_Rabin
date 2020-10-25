3bd8bcb5505502f7d6cae6e041f3f646921478b6
Dawid Zawadzki <lamazlo21@gmail.com>
2020-10-25 11:29:13 +0100

    Remake TextSplitter class for better performance
    1. Add numOfWindows method to calculate number of windows to examine in text
    2. Add lenOfChain method to calculate number of chains for one thread and number
       of additional windows
    3. Refactor code of splitText method to make it more efficient and easier to handle by removing unneccesary if statments

--- header/Utility/TextSplitter.h
+++ header/Utility/TextSplitter.h
@@ -14,9 +14,10 @@ class TextSplitter {
 private:
     static const int numOfThreads = 32;
 
-    int rangeWidth(long long int textLength);
+    int numOfWindows(long long int textLength, int patternLength);
+    std::pair<int, int> lenOfChain(int windows);
 public:
-    std::vector<std::tuple<size_t, size_t>>& splitText(long long int textLength, int patternLength);
+    std::vector<std::tuple<size_t, size_t>> splitText(long long int textLength, int patternLength);
 
 };
 
--- src/Utility/TextSplitter.cpp
+++ src/Utility/TextSplitter.cpp
@@ -1,22 +1,36 @@
 #include "TextSplitter.h"
+#include <iostream>
+int TextSplitter::numOfWindows(long long int textLength, int patternLength) {
+    return textLength - patternLength + 1;
+}
 
-int TextSplitter::rangeWidth(long long int textLength) {
-    return ceil((double) textLength / (double) numOfThreads);
+std::pair<int, int> TextSplitter::lenOfChain(int windows) {
+    int rangeWidth = windows / numOfThreads;
+    int rangesWithBonus = windows % numOfThreads;
+    return std::make_pair(rangeWidth, rangesWithBonus - 1);
 }
 
 // It's hard cut the text into substrings of equal size without getting out of range sometimes,
 // so when we get out of range a <-1, -1> is pushed back to indicate that this tuple is wrong and thread should not process it
 
-std::vector<std::tuple<size_t, size_t>>& TextSplitter::splitText(long long int textLength, int patternLength) {
-    int width = rangeWidth(textLength);
-
-    for(int i = 0; i < textLength; i += width) {
-        if((i + patternLength - 1) < textLength && (i + width - 1 + patternLength - 1) < textLength) {
-        }
-        else if((i + patternLength - 1) < textLength && ((i + width - 1 + patternLength - 1) >= textLength)) {
-        } else if((i + patternLength - 1) >= textLength) {
+std::vector<std::tuple<size_t, size_t>> TextSplitter::splitText(long long int textLength, int patternLength) {
+    std::vector<std::tuple<size_t, size_t>> ranges;
+    int windows = numOfWindows(textLength, patternLength);
+    std::pair<int, int> chains = lenOfChain(windows);
+    int currenPosition = 0;
+    for(int i = 0; i < numOfThreads; i++) {
+        if(windows > i) {
+            if (chains.second >= 0) {
+                ranges.push_back(std::make_tuple(currenPosition, currenPosition + chains.first));
+                currenPosition = currenPosition + chains.first + 1;
+                chains.second--;
+            } else {
+                ranges.push_back(std::make_tuple(currenPosition, currenPosition + chains.first - 1));
+                currenPosition = currenPosition + chains.first;
+            }
+        } else {
+            break;
         }
     }
-
-
+    return std::move(ranges);
 }
