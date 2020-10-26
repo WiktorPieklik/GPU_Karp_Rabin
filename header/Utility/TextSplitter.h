d111c6d051475cdfb7ba171d10c1a6af0b64df3d
Dawid Zawadzki <lamazlo21@gmail.com>
2020-10-26 08:43:48 +0100

    Change name of 'windows' variable to 'numOfWindows' to avoid misleading naming

--- header/Utility/TextSplitter.h
+++ header/Utility/TextSplitter.h
@@ -10,7 +10,7 @@ private:
     static const int numOfThreads = 32;
 
     size_t numOfWindows(size_t textLength, int patternLength);
-    std::pair<int, int> lenOfChain(int windows);
+    std::pair<int, int> lenOfChain(int numOfWindows);
 public:
     std::vector<std::pair<size_t, size_t>> splitText(long long int textLength, int patternLength);
 
--- src/Utility/TextSplitter.cpp
+++ src/Utility/TextSplitter.cpp
@@ -4,19 +4,19 @@ size_t TextSplitter::numOfWindows(size_t textLength, int patternLength) {
     return textLength - patternLength + 1;
 }
 
-std::pair<int, int> TextSplitter::lenOfChain(int windows) {
-    int rangeWidth = windows / numOfThreads;
-    int rangesWithBonus = windows % numOfThreads;
+std::pair<int, int> TextSplitter::lenOfChain(int numOfWindows) {
+    int rangeWidth = numOfWindows / numOfThreads;
+    int rangesWithBonus = numOfWindows % numOfThreads;
     return std::make_pair(rangeWidth, rangesWithBonus);
 }
 
 std::vector<std::pair<size_t, size_t>> TextSplitter::splitText(long long int textLength, int patternLength) {
     std::vector<std::pair<size_t, size_t>> ranges;
-    int windows = numOfWindows(textLength, patternLength);
-    std::pair<int, int> chains = lenOfChain(windows);
+    int _numOfWindows = numOfWindows(textLength, patternLength);
+    std::pair<int, int> chains = lenOfChain(_numOfWindows);
     int currenPosition = 0;
     for(int i = 0; i < numOfThreads; i++) {
-        if(windows > i) {
+        if(_numOfWindows > i) {
             if (chains.second > 0) {
                 ranges.emplace_back(std::make_pair(currenPosition, currenPosition + chains.first));
                 currenPosition = currenPosition + chains.first + 1;
